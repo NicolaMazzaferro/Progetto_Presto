cedec5fde2a6fdd714dea48e3d2efb093ef2cc92
gabri <gabrigsec@hotmail.it>
2023-08-16 15:50:13 +0200

    recap-struttura front

---
+++ resources/views/components/our-team.blade.php
@@ -0,0 +1,57 @@
+<div class="container container-our-team">
+        <div class="row text-center">
+            <div class="col-12 text-arancio">
+                <h5 class="fs-2">// IL NOSTRO TEAM</h5>
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="col-12 text-nero mt-5 d-flex justify-content-center">
+                <h1 class=" title-last-product titolo-au fw-bolder">Il nostro Team e Le Nostre Skills</h1>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-3 mb-5">
+                <div class="card card-our-team">
+                    <img src="./media/im1.jpeg" class="card-img-top img-card-our-team" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Nicola Mazzaferro</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Full Stack</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 mb-5">
+                <div class="card card-our-team">
+                    <img src="./media/im2.jpeg" class="card-img-top img-card-our-team" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Ilaria Amorotti</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Front-end</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 mb-5">
+                <div class="card card-our-team">
+                    <img src="./media/im4.jpeg" class="card-img-top img-card-our-team" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Stefano Grandi</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Front-end</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 mb-5">
+                <div class="card card-our-team">
+                    <img src="./media/im3.jpeg" class="card-img-top img-card-our-team" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Gabriele Quiroz</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Full Stack</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
