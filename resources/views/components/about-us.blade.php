69ee5f9ca3020d419cf72aab033b4d5cf21a971a
NicolaMazzaferro <nicolamazzaferro9@gmail.com>
2023-08-19 18:27:42 +0200

    Aggiunto collegamento a button in home lavora con noi e testo della relativa sezione

--- resources/views/components/about-us.blade.php
+++ resources/views/components/about-us.blade.php
@@ -15,19 +15,19 @@
             <div class="col-12 col-md-2">
                 
             </div>
-            <div class="col-12 col-md-5 wrapper-about-us">
-                <h5 class="text-uppercase text-arancio fs-2 mb-4">// About us</h5>
-                <h2 class="titolo-au text-uppercase fw-bolder">Traendo ispirazione dalle ultime tendenze</h2>
-                <p class="fs-5 fw-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi itaque culpa ipsa porro dolorum, similique repellat qui commodi unde facere ullam adipisci est, tempora deleniti eveniet natus cum dolor nostrum!</p>
-                <p class="fs-5 fw-medium">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati quae id error ea veritatis corrupti perspiciatis ipsa optio illo eos?</p>
+            <div class="col-12 col-md-5 ">
+                <h5 class="text-uppercase text-arancio fs-2 mb-4">// lavora con noi</h5>
+                <h2 class="titolo-au text-uppercase fw-bolder">Unisciti alla nostra squadra dinamica!</h2>
+                <p class="fs-5 fw-medium">Entra a far parte della nostra famiglia in crescita! Se sei appassionato di innovazione e vuoi fare parte di un team che crea esperienze uniche, sei nel posto giusto. Offriamo opportunità stimolanti e uno spazio per far crescere le tue competenze.</p>
+                <p class="fs-5 fw-medium">Condividi la tua passione per l'eccellenza e contribuisci a plasmare il futuro del commercio online. Scopri le posizioni aperte e unisciti a noi nell'offrire ai nostri clienti qualità e servizio straordinari.</p>
                 <div class="row">
                     <div class="col-3">
-                        <p class="fs-5"><i class="fa-solid fa-check text-arancio fs-4 me-3"></i> Lorem</p>
-                        <p class="fs-5"><i class="fa-solid fa-check text-arancio fs-4 me-3"></i> Lorem</p>
+                        <p class="fs-5 d-flex"><i class="fa-solid fa-check text-arancio fs-4 me-3"></i> Collaborazione</p>
+                        <p class="fs-5 d-flex"><i class="fa-solid fa-check text-arancio fs-4 me-3"></i> Sviluppo</p>
                     </div>
-                    <div class="col-3">
-                        <p class="fs-5"><i class="fa-solid fa-check text-arancio fs-4 me-3"></i> Lorem</p>
-                        <p class="fs-5"><i class="fa-solid fa-check text-arancio fs-4 me-3"></i> Lorem</p>
+                    <div class="col-3 mx-5 px-5">
+                        <p class="fs-5 d-flex"><i class="fa-solid fa-check text-arancio fs-4 me-3"></i> Impatto</p>
+                        <p class="fs-5 d-flex"><i class="fa-solid fa-check text-arancio fs-4 me-3"></i> Innovazione</p>
                     </div>
                 </div>
                 
@@ -37,9 +37,9 @@
                     </a>
                 </div> --}}
 
-                <div class="btn-nicola">
+                <a href="{{route("workWithUs")}}" class="btn-nicola">
                     DETTAGLI
-                  </div>
+                </a>
             </div>
         </div>
     </section>
