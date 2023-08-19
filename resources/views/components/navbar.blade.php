44c86ca7cde838f10c635ac2ed2730a1cc740770
NicolaMazzaferro <nicolamazzaferro9@gmail.com>
2023-08-19 21:16:45 +0200

    Aggiunta logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,12 +2,12 @@
     
     <nav id="navbar" class="navbar navbar-expand-lg fixed-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/">LOGO</a>
+            <a class="navbar-brand" href="/"><img src="{{asset('./media/logo.svg')}}" alt="LOGO"></a>
             <button class="navbar-toggler mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fa-solid fa-bars menu-burger"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fs-5">
                     <li class="nav-item">
                         <a class="nav-link " aria-current="page" href="/">Home</a>
                     </li>
