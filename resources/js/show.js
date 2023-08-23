0504b419dc275dbc4e5e7f5f83ca9d5c4b72b6a0
stefano grandi <stefano.grandi1995@gmail.com>
2023-08-23 23:00:37 +0200

    pagina annunci

---
+++ resources/js/show.js
@@ -0,0 +1,13 @@
+
+var swiper1 = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    thumbs: {
+        swiper: swiper1,
+    },
+});
--- resources/views/announcement/show.blade.php
+++ resources/views/announcement/show.blade.php
@@ -1,16 +1,85 @@
 <x-layout> 
-    <div class="container bg-bianco p-5">
+    <div class="container  bg-bianco p-5">
         <h1 class="text-center mb-5">DETTAGLI PRODOTTO</h1>
         <div class="row">
             
             <div class="col-12 col-md-6">
-                {{-- Carousel --}}
+                
+
+                <div class="swiper mySwiper2">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
+                        </div>
+                    </div>
+                    
+                </div>
+                <div thumbsSlider="" class="swiper mySwiper">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+                        </div>
+                        <div class="swiper-slide">
+                            <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
+                        </div>
+                    </div>
+                </div>
+
             </div>
             
-            <div class="col-12 col-md-6">
-                <h5 class="card-title text-uppercase text-arancio pt-3">{{$announcement->title}}</h5>
-                <p class="lead pt-3 text-nero">{{$announcement->description}}</p>
-                <p class="fw-semibold text-nero">€{{$announcement->price}}</p>
+            <div class="col-12 col-md-6 dettaglio">
+                <h5 class="card-title text-uppercase  text-arancio pt-3">{{$announcement->title}}</h5>
+                <p class="lead pt-3 text-nero ">{{$announcement->description}}</p>
+                <p class="fw-semibold text-nero ">€{{$announcement->price}}</p>
+                
             </div>
         </div>
     </div>
