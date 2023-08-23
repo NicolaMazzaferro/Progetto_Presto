e6fe4fd629c3101e1209590a684f3fa86d8c026a
Ilaria Amorotti <ilaria.amorotti97@gmail.com>
2023-08-23 22:35:12 +0200

    reject.blade

--- resources/views/announcement/reject.blade.php
+++ resources/views/announcement/reject.blade.php
@@ -2,9 +2,9 @@
     <x-offcanva></x-offcanva>
     <div class=" rejected-ads container p-5">
         @if (count($announcement_reject))
-            <h1 class="pb-5 text-center">Ecco l'ultimo annuncio rifiutato</h1>
+        <h1 class="pb-5 text-center">Ecco l'ultimo annuncio rifiutato</h1>
         @else
-            <h1 class="pb-5 text-center">Non ci sono annunci rifiutati</h1>
+        <h1 class="pb-5 text-center">Non ci sono annunci rifiutati</h1>
         @endif
         @if ($announcement_reject)
         
@@ -16,26 +16,26 @@
                     {{-- carousel --}}
                     <div id="{{$item->id}}" class="carousel slide">
                         @if(count($item->images))
-                            <div class="carousel-inner h-25">
-                                @foreach ($item->images as $image)
-                                <div class="carousel-item @if ($loop->first)active @endif">
-                                    <img src="{{Storage::url($image->path)}}" class="card-img-top" height="350px" alt="...">
-                                </div>
-                                @endforeach
+                        <div class="carousel-inner h-25">
+                            @foreach ($item->images as $image)
+                            <div class="carousel-item @if ($loop->first)active @endif">
+                                <img src="{{Storage::url($image->path)}}" class="card-img-top" height="350px" alt="...">
                             </div>
-                            @else
-                            <img src="\storage\default.jpg" class="card-img-top" height="350px" alt="...">
-                            @endif
-                            <button class="carousel-control-prev" type="button" data-bs-target="#{{$item->id}}" data-bs-slide="prev">
-                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                              <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#{{$item->id}}" data-bs-slide="next">
-                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                              <span class="visually-hidden">Next</span>
-                            </button>
-                          </div>
-                          {{-- end carousel --}}
+                            @endforeach
+                        </div>
+                        @else
+                        <img src="\storage\default.jpg" class="card-img-top" height="350px" alt="...">
+                        @endif
+                        <button class="carousel-control-prev" type="button" data-bs-target="#{{$item->id}}" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#{{$item->id}}" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                    {{-- end carousel --}}
                     <div class="card-body">
                         <h5 class="card-title">{{$item->title}}</h5>
                         <p class="card-text">{{$item->description}}</p>
