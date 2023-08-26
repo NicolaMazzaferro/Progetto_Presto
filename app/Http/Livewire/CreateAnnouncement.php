521d1ab57b02e1c23ec575eee180e9a4ab7f5c09
gabri <gabrigsec@hotmail.it>
2023-08-26 11:25:34 +0200

    US-8

--- app/Http/Livewire/CreateAnnouncement.php
+++ app/Http/Livewire/CreateAnnouncement.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use Illuminate\Support\Facades\File;
 use Livewire\Component;
@@ -98,9 +99,11 @@ class CreateAnnouncement extends Component
                 $newFileName = "announcements/{$announcement->id}";
                 $newImage = $announcement->images()->create(['path'=>$image->store($newFileName, 'public')]);
 
-                dispatch(new ResizeImage($newImage->path , 300, 300));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+                RemoveFaces::withChain([
+                    (new ResizeImage($newImage->path , 300, 300)),
+                    (new GoogleVisionSafeSearch($newImage->id)),
+                    (new GoogleVisionLabelImage($newImage->id))
+                ])->dispatch($newImage->id);
             }
 
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
