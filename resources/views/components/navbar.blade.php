0089cdeb6042e2f350e5a793c77af96850715072
stefano grandi <stefano.grandi1995@gmail.com>
2023-08-27 15:33:58 +0200

    modifiche

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -16,10 +16,10 @@
                 <div class="nav-item" id="box-search">
                     <form class="d-flex" role="search" action="{{ route('announcement_search') }}" method="get">
                         <input class="form-control me-2" name="searched" type="search" placeholder="Cerca..." aria-label="Search">
+                        <button class="btn btn-arancio d-flex align-items-center" type="submit">
+                            <i class="bi bi-search text-bianco fs-5"></i>
+                        </button>
                     </form>
-                    <button class="btn btn-arancio d-flex align-items-center" type="submit">
-                        <i class="bi bi-search text-bianco fs-5"></i>
-                    </button>
                 </div>
 
                 {{-- search-bar end --}}
